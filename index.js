74b357edf3686515c9d0bcd50bf6fc657b51cf4d
Sidney schneider <sidney@camerite.com>
2021-04-15 13:17:22 -0300

    added body parser

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 require('./routes')(app);
 
@@ -7,5 +8,6 @@ app.use('/public', express.static('public'));
 app.set('views', __dirname + '/public/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000);
