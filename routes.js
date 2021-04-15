239084992eb402fae0b1508f87283ddb01d53179
Sidney schneider <sidney@camerite.com>
2021-04-15 13:21:06 -0300

    indent

--- routes.js
+++ routes.js
@@ -6,32 +6,32 @@ module.exports = (app) => {
     app.get('/', (req, res) => {
         res.render('index.html');
     });
-
+    
     app.get('/api/cameras', async (req, res) => {
         await errortest.shouldThrowError(req, res); //Do not change, used for test purposes
-
+        
         res.status(200).json(db.cameras);
     });
-
+    
     app.delete('/api/cameras/:id', async (req, res) => {
         await errortest.shouldThrowError(req, res); //Do not change, used for test purposes
-
+        
         console.log('Received id:', req.params.id);
         //TODO Delete camera from db
-
+        
         res.status(200);
     });
-
-
+    
+    
     app.post('/api/contact', async (req, res) => {
         await errortest.shouldThrowError(req, res); //Do not change, used for test purposes
-
-
+        
+        
         //TODO Insert contact info into db.contacts
-
+        
         res.status(200).end();
     });
-
+    
     //Do not change, used for test purposes
     app.get('/api/contact', (req, res) => {
         res.status(200).json(db.contacts);
